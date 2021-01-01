fadff8fff45ef5d41ff3f7462baefc635882545a
Dyrits <dyrits@outlook.com>
2021-01-01 11:22:01 +0100

    Initial commit.

---
+++ mixed_messages.js
@@ -0,0 +1,22 @@
+const data = {
+    characters: ["Doggy the dog", "Catty the cat", "Dylan the human"],
+    feelings: ["likes", "dislikes", "loves", "is happy to", "is worried", "is afraid"],
+    actions: ["eat", "bark at", "sleep on", "look at", "laugh at"],
+    objects: ["spaghetti", "stranger", "needles", "horses", "bed", "red pepper"]
+};
+
+const getElementFrom = array => array[Math.floor(Math.random() * array.length)];
+
+
+mixedMessages = [];
+const {feelings, actions, objects} = data;
+
+data.characters.forEach(character => {
+    mixedMessages.push(`${character} ${getElementFrom(feelings)} to ${getElementFrom(actions)} ${getElementFrom(objects)}.`)
+})
+
+const display = messages => {
+    console.log(messages.join("\n"));
+}
+
+display(mixedMessages);
