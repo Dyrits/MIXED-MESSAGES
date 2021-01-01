b8c7662222362ebc3c6437f9696cd6792aa7f13e
Dyrits <dyrits@outlook.com>
2021-01-01 11:24:27 +0100

    Code refactored.

--- mixed_messages.js
+++ mixed_messages.js
@@ -7,16 +7,16 @@ const data = {
 
 const getElementFrom = array => array[Math.floor(Math.random() * array.length)];
 
-
 mixedMessages = [];
-const {feelings, actions, objects} = data;
 
-data.characters.forEach(character => {
-    mixedMessages.push(`${character} ${getElementFrom(feelings)} to ${getElementFrom(actions)} ${getElementFrom(objects)}.`)
-})
-
-const display = messages => {
-    console.log(messages.join("\n"));
+const fillMessages = data => {
+    const {feelings, actions, objects} = data;
+    data.characters.forEach(character => {
+        mixedMessages.push(`${character} ${getElementFrom(feelings)} to ${getElementFrom(actions)} ${getElementFrom(objects)}.`)
+    })
 }
 
+const display = messages => { console.log(messages.join("\n")); }
+
+fillMessages(data);
 display(mixedMessages);
